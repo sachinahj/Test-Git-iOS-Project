2b3f7d02deee32555fe7cdc201fb0a2287a79b5c
Sachin Ahuja <Sachin@sachin-ahujas-macbook-pro.local>
2014-03-09 01:34:24 -0600

    added a test class

---
+++ SATest.h
@@ -0,0 +1,13 @@
+//
+//  SATest.h
+//  Git Test
+//
+//  Created by Sachin Ahuja on 3/9/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SATest : NSObject
+
+@end
---
+++ SATest.m
@@ -0,0 +1,13 @@
+//
+//  SATest.m
+//  Git Test
+//
+//  Created by Sachin Ahuja on 3/9/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import "SATest.h"
+
+@implementation SATest
+
+@end
